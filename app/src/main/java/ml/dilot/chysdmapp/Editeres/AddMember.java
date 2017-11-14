023b234bdc99431f8a65cef56569f2a6117b0415
Mellbrown <mlyg2588@hotmail.com>
2017-11-14 21:35:59 +0900

    AddMember 액티비티 추가(미완)

---
+++ app/src/main/java/ml/dilot/chysdmapp/Editeres/AddMember.java
@@ -0,0 +1,15 @@
+package ml.dilot.chysdmapp.Editeres;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ml.dilot.chysdmapp.R;
+
+public class AddMember extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_member);
+    }
+}
